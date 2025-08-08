e45c795e46098d4884d105c3fd5c37464e2492b1
andrewbakradze <asmart94317@gmail.com>
2025-08-08 16:04:59 +0900

    first commit

---
+++ src/main/java/com/AndrewB/demo/controllers/MainController.java
@@ -0,0 +1,28 @@
+package com.AndrewB.demo.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting( Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+    @GetMapping("/about")
+    public String about( Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+    @GetMapping("/contact")
+    public String contact ( Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+}
